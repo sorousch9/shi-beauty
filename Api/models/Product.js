daa2ccff41ecf1c60f74df5d8974b42ec99bce9a
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-09-13 14:30:04 +0200

    1

--- Api/models/Product.js
+++ Api/models/Product.js
@@ -3,16 +3,14 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true},
+    desc: { type: String, required: true },
     img: { type: String, required: true },
-    categories: { type: Array},
-    size: { type: String},
-    color: { type: String},
+    categories: { type: Array },
+    size: { type: Array },
+    color: { type: Array },
     price: { type: Number, required: true },
-    
+    instoc: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 module.exports = mongoose.model("Product", ProductSchema);
-
-
