53b7b9b421a9b0dc73de0a9b98cb5c87231393d0
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-09-13 10:35:55 +0200

    a

---
+++ Client/src/Data.js
@@ -0,0 +1,141 @@
+import slide1 from "./assets/Gendersplit.webp";
+import slide2 from "./assets/FS22_neu.webp";
+import slide3 from "./assets/filling_August_52.webp";
+import cat1 from "./assets/shi Beauty (1).webp";
+import cat2 from "./assets/shi Beauty (2).webp";
+import cat3 from "./assets/shi Beauty (3).webp";
+import cat4 from "./assets/shi Beauty (4).webp";
+import cat5 from "./assets/shi Beauty (5).webp";
+import cat6 from "./assets/shi Beauty (6).webp";
+import cat7 from "./assets/shi Beauty (7).webp";
+import cat8 from "./assets/shi Beauty (8).webp";
+import PImage1 from "./assets/P1.webp";
+import PImage2 from "./assets/P2.webp";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: slide1,
+    title: "Ein Herz für Mode",
+    desc: "Endecke aufregende und hochwertige Fashion-Schätze in unserer Pre-owned Kollektion",
+    backColor:"radial-gradient(circle, rgba(184,238,174,1) 0%, rgba(255,255,255,1) 100%)"
+  },
+  {
+    id: 2,
+    img: slide2,
+    title: "Die Klassiker der Herbstsaison",
+    desc: "GEHEN SIE KEINE KOMPROMISSE BEIM STIL! ERHALTEN SIE 30 % RABATT FÜR NEUE ANKÜNFTE.",
+    backColor:"radial-gradient(circle, rgba(174,175,238,1) 0%, rgba(255,255,255,1) 100%)"
+  },
+  {
+    id: 3,
+    img: slide3,
+    title: "Style Report",
+    desc: "Lass dich für die neue Saison inspirieren und frische deinen Look mit aktuellen Must-haves auf.",
+    backColor:"radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(255,255,255,1) 100%)"
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/2916814/pexels-photo-2916814.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Charmeuse skirt!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/995978/pexels-photo-995978.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Cardigan STYLE!",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/3754166/pexels-photo-3754166.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Galloon",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Houndstooth jacket",
+  },
+];
+export const popularProducts = [
+  {
+    id: 1,
+    name:"Buffalo Jerseykleid mit Blümchenprint",
+    price:39.99,
+    img: cat1,
+    size:36,
+    color:"pink"
+  },
+  {
+    id: 2,
+    name:"Alba Moda Strandkleid ",
+    price:92.95,
+    img: cat2,
+    size:32,
+    color:"peach"
+  },
+  {
+    id: 3,
+    name:"Alba Moda Strandkleid »Strandkleid",
+    price:35,
+    img: cat3,
+    size:40,
+    color:"teal"
+  },
+  {
+    id: 4,
+    name:"Alba Moda Strandkleid ",
+    price:28.99,
+    img: cat4,
+    size:42,
+    color:"gray"
+  },
+  {
+    id: 5,
+    name:"s.Oliver Strandkleid mit zartem Blumenprint",
+    price:46.99,
+    img: cat5,
+    size:38,
+    color:"brown"
+  },
+  {
+    id: 6,
+    name:"LASCANA Midikleid (mit Gürtel) mit Punktedruck",
+    price:75.99,
+    img: cat6,
+    size:34,
+    color:"lime"
+  },
+  {
+    id: 7,
+    name:"Vivance Jerseykleid mit Blümchenprint",
+    price:79.99,
+    img: cat7,
+    size:36,
+    color:"green"
+  },
+  {
+    id: 8,
+    name:"Calvin Klein Jeans Plus Jerseykleid",
+    price:33.99,
+    img: cat8,
+    size:40,
+    color:"orange"
+  },
+  {
+    id: 9,
+    name:"GOLDNER Druckkleid",
+    price:36.99,
+    img: PImage1,
+    size:30,
+    color:"purple"
+  },
+  {
+    id: 10,
+    name:"Melrose Midikleid",
+    price:31.99,
+    img: PImage2,
+    size:38,
+    color:"red"
+  },
+];
---
+++ Client/src/pages/Cart.jsx
@@ -0,0 +1,331 @@
+import styled from "styled-components";
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import { Anons } from "../components/Anons";
+import PImage1 from "../assets/P1.webp";
+import PImage2 from "../assets/P2.webp";
+import Icons from "../assets/singleV3.png";
+import { Add, Remove } from "@mui/icons-material";
+import { mobile } from "../responsive";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  margin-bottom: 20px;
+  ${mobile({ marginTop: "20px" })};
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })};
+`;
+const TopButton = styled.button`
+  padding: 2px;
+  outline: 0;
+  font-size: 17px;
+  color: rgb(0, 0, 0);
+  background: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    from(#fdde5c),
+    color-stop(#f8ab5e),
+    color-stop(#f56a62),
+    color-stop(#a176c8),
+    color-stop(#759beb),
+    color-stop(#65beb3),
+    to(#70db96)
+  );
+  background: linear-gradient(
+    to right,
+    #fdde5c,
+    #f8ab5e,
+    #f56a62,
+    #a176c8,
+    #759beb,
+    #65beb3,
+    #70db96
+  );
+  border-radius: 15px;
+  border: 0;
+  box-shadow: none;
+  cursor: pointer;
+  margin-bottom: 10px;
+  ${mobile({ display: "none" })};
+`;
+const ButtenText = styled.span`
+  display: block;
+  padding: 10px 40px;
+  font-size: 17px;
+  font-weight: 500;
+  background: ${(props) => (props.type === "filled" ? "#ebebeb" : "#white")};
+  border-radius: 15px;
+`;
+
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })};
+`;
+const TopText = styled.span`
+  cursor: pointer;
+  margin: 0px 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 110px;
+  ${mobile({ margin: "10px" })}
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.div``;
+const ProductId = styled.span``;
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.span``;
+const PriceDetail = styled.span`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  border:1px solid #ececec;
+  border-radius 5px;
+  background-color: #ececec;
+  padding: 0px 10px;
+  cursor: pointer;
+  ${mobile({ margin: "10px" })}
+`;
+const ProductAmount = styled.div`
+  font-size: 20px;
+  margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
+`;
+
+const ProductPrice = styled.div`
+  font-size: 25px;
+  font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
+`;
+const Gap = styled.hr`
+  background-color: #bebebe;
+  border: none;
+  height: 1px;
+  margin: 10px;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const SummaryButton = styled.button`
+  width: 100%;
+  padding: 2px;
+  outline: 0;
+  font-size: 17px;
+  color: rgb(0, 0, 0);
+  background: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    from(#fdde5c),
+    color-stop(#f8ab5e),
+    color-stop(#f56a62),
+    color-stop(#a176c8),
+    color-stop(#759beb),
+    color-stop(#65beb3),
+    to(#70db96)
+  );
+  background: linear-gradient(
+    to right,
+    #fdde5c,
+    #f8ab5e,
+    #f56a62,
+    #a176c8,
+    #759beb,
+    #65beb3,
+    #70db96
+  );
+  border-radius: 15px;
+  border: 0;
+  box-shadow: none;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+const SummaryButtonText = styled.span`
+  display: block;
+  padding: 10px 40px;
+  font-size: ${(props) => props.type === "total" && "24px"};
+  font-weight: ${(props) => props.type === "total" && "500"};
+  background: #ebebeb;
+  border-radius: 15px;
+`;
+
+const UnderBottom = styled.div``;
+const IconsContainerTitle = styled.h3`
+  margin-bottom: 10px;
+`;
+const IconsContainer = styled.img`
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+export const Cart = () => {
+  return (
+    <Container>
+      <Anons />
+      <Navbar />
+      <Wrapper>
+        <Title>Warenkorb</Title>
+        <Top>
+          <TopButton>
+            <ButtenText type="filled">Weiter einkaufen</ButtenText>
+          </TopButton>
+          <TopTexts>
+            <TopText>Warenkorb (2)</TopText>
+            <TopText>Merkzettel (0) </TopText>
+          </TopTexts>
+          <TopButton>
+            <ButtenText type="filled">Zur Kasse</ButtenText>
+          </TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src={PImage1} />
+                <Details>
+                  <ProductName>
+                    <b>Product :</b>Vivance Hosenrock mit Blumendruck
+                  </ProductName>
+                  <ProductId>
+                    <b>ID :</b>984651354546
+                  </ProductId>
+                  <ProductColor color="pink" />
+                  <ProductSize>
+                    <b>Size:</b> 38
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>29.99 €</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Gap />
+            <Product>
+              <ProductDetail>
+                <Image src={PImage2} />
+                <Details>
+                  <ProductName>
+                    <b>Product :</b>LASCANA Wickelrock mit Alloverdruck
+                  </ProductName>
+                  <ProductId>
+                    <b>ID :</b>984651365987
+                  </ProductId>
+                  <ProductColor color="purple" />
+                  <ProductSize>
+                    <b>Size:</b> 38
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>49.99 €</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryItem>
+              <SummaryTitle>Ihr Einkauf</SummaryTitle>
+              <SummaryItemText>Warenwert regulär</SummaryItemText>
+              <SummaryItemPrice> 109.97 €</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Versandkosten</SummaryItemText>
+              <SummaryItemPrice>3.99 €</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Versandkosten Rabatt</SummaryItemText>
+              <SummaryItemPrice>-3.99 €</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Gesamtsumme</SummaryItemText>
+              <SummaryItemPrice>109.97 €</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryButton>
+              <SummaryButtonText>ZUR KASSE</SummaryButtonText>
+            </SummaryButton>
+            <UnderBottom>
+              <IconsContainerTitle>WIR AKZEPTIEREN:</IconsContainerTitle>
+              <IconsContainer src={Icons} />
+            </UnderBottom>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
